0c1eaba9941361a0ef5975c22fccc85c2d4e402e
wicaksonoindra <wicaksonoindra66@gmail.com>
2023-01-02 21:03:36 +0700

    add eps 37

---
+++ 37-tuples-dan-set.py
@@ -0,0 +1,17 @@
+# List
+data_list = [10,2,4,3,2] # menggunakan kurung siku
+print(data_list)
+# Tuples
+# Isinya tidak bisa di rubah
+data_tuples = (7,8,9,10)
+print(data_tuples)
+print(data_tuples[1])
+
+# data_tuples[1] = "ucup" # tuples tidak bis dirubah isinya
+# data_tuples.append(1) # tuples juga tidak bisa di tambahkan
+
+# Sets(himpunan)
+# datanya tidak memiliki index, index tidak penting disini
+data_sets = {10,4,3,2,4,7,6,5}
+print(data_sets)
+print(data_sets[0]) # sets tidak memiliki index
