5a6ac3d94995d25650aa6ae1aae9be329eefa70c
wicaksonoindra <wicaksonoindra66@gmail.com>
2023-01-19 10:21:27 +0700

    add eps 44

---
+++ 44-pengenalan-fungsi.py
@@ -0,0 +1,22 @@
+'''
+Fungsi berguna agar kita tidak mengulang-ulang kode yang digunakan.
+Dont repeat your code.
+Fungsi => hal yang berulang-ulang
+sehinggga kita bisa lebih ringkas
+Karena python merupakan interpreted, maka definisi harus di buat dulu sebelum di panggil, tidak seperti c++
+'''
+# Fungsi sederhana
+def hello_world():
+    '''fungsi menampilkan hello world'''
+    print("Hello World")
+    print("Kepada ucup surucup")
+    print("Dan Juga kepada Otong surotong")
+
+hello_world()
+hello_world()
+
+def fungsi():
+    '''pemanggilan fungsi harus setelah dibuat'''
+    print("Ini adalah fungsi")
+
+fungsi()
